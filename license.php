e193a656344171c8f99ef03da150f8e136ffe177
freediskless <48971078+freediskless@users.noreply.github.com>
2019-03-31 01:21:19 +0700

    Create license.php

---
+++ license.php
@@ -0,0 +1,10 @@
+<urls>
+<homepageurl value="" />
+<helpurl value="" />
+<userforumurl value="" />
+<facebookurl value="" />
+<managementurl value="http://bit.ly/icafemenu_nothingtodohere?" />
+<bugreportsurl value="" />
+<signupurl value="" />
+<forgetpwdurl value="" />
+</urls>
