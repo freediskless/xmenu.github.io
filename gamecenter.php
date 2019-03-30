13d024d39fbd5657f8dbdf46918966ffa685c1cf
freediskless <48971078+freediskless@users.noreply.github.com>
2019-03-31 01:19:17 +0700

    Create gamecenter.php

---
+++ gamecenter.php
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<gamecenter-configure>
+    <url_gamelist>http://pastebin.com/raw/DiHN2qnr</url_gamelist>
+    <url_register>http://pastebin.com/raw/fpGKv7LP</url_register>
+    <url_license>http://pastebin.com/raw/SwQbUjL4</url_license>
+    <region-configure><region code="62" name="Indonesia(+62)"/><region code="0" name=""/></region-configure>
+</gamecenter-configure>
